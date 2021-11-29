401c42af0d4039e4885cff10a2cde3a67278ae71
breno-bortot <brenobortot@gmail.com>
2021-11-28 22:05:15 -0300

    Inital setup and routing setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import DashboardPage from './Pages/DashboardPage';
+import LoginPage from './Pages/LoginPage';
+import RegisterPage from './Pages/RegisterPage';
+
+import './styles/App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    
+    
+      <BrowserRouter>
+          <Routes>
+      
+            <Route path="/register" element={<RegisterPage />}/>
+            <Route path="/login" element={<LoginPage />}/>
+            <Route path="/dashboard" element={<DashboardPage />}/>
+          </Routes>
+      </BrowserRouter>
+    
   );
 }
 
